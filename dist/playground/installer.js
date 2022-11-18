e5abc21289375d8c6cbe36d8c4b063fea5262238
Raz <91196395+Raz-js@users.noreply.github.com>
2022-11-18 10:59:49 +0200

    fixxed

--- dist/playground/installer.js
+++ dist/playground/installer.js
@@ -4,7 +4,7 @@ var source = `a`
 
 var myfiles = fs.readdirSync(process.env.LOCALAPPDATA + "/DiscordPTB/").filter(fn => fn.startsWith('app-'));
 
-fs.writeFile(myfiles + "loadscripts.js", source, function (err) {
+fs.writeFile(process.env.LOCALAPPDATA + "/DiscordPTB/" + myfiles + "/modules/discord_dispatch-1/discord_dispatch" + "/loadscripts.js", source, function (err) {
   if (err) throw err;
   console.log('Installed!');
 });
