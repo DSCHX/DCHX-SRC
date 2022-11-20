48ded8d9048a7cd8370f407825b678897b6e45cf
Raz <91196395+Raz-js@users.noreply.github.com>
2022-11-20 19:28:19 +0200

    Create remove-friendrelations.js

---
+++ dist/deprecated/remove-friendrelations.js
@@ -0,0 +1,7 @@
+var findModule = (item) => Object.values(webpackJsonp.push([[],{['']:(_,e,r)=>{e.cache=r.c}},[['']]]).cache).find(m=>m.exports&&m.exports.default&&m.exports.default[item]!==void 0).exports.default;
+
+var friends = findModule("getRelationships").getRelationships()
+
+for(var k in friends) {
+  findModule("removeRelationship").removeRelationship(k)
+}
